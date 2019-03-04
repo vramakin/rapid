1b506de6e5b3a6b36c5e4468f179981a274a06e8
Ram <kikkupico@gmail.com>
2019-03-04 13:43:26 +0530

    trying out GUI ui creation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Row, Col, Button} from 'antd'
+import {layout1, layout2} from './Samples'
 
 class App extends Component {
+  state = {code:<strong>Hello World!</strong>}
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+      <Row>
+      <Col >
+      <Button onClick={()=>{console.log('clicked');this.setState({code:layout1})}} >1</Button>
+      <Button onClick={()=>{console.log('clicked');this.setState({code:layout2})}} >2</Button>
+      </Col>
+      </Row>    
+      {this.state.code}     
       </div>
     );
   }

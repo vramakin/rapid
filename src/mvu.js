58b2516521104f500e4af87222aa078d451b41e5
Ram <kikkupico@gmail.com>
2019-03-22 18:52:07 +0530

    some trials for dynamic ui component editing

--- src/mvu.js
+++ src/mvu.js
@@ -1,27 +1,7 @@
 import React from "react";
 import { DragSource } from 'react-dnd';
 
-export var lastId = 4;
-
-var UITREE = {
-	1: {
-		type: "div",
-		props: {style:{backgroundColor:'red', padding:"1em"}},
-		children: {
-			2: {
-				type: "div",
-				props: {style:{backgroundColor:'blue'}},
-				children: { 3: { type: "text", value: "it works" } }
-			},
-			4: {
-				type: "div",
-				props: null,
-				children: { 5: { type: "text", value: "deletion works" } }
-			}
-		}
-	}
-}
-
+let lastId = 10
 
 const getComponent = e => {
 	const genericDragSource = {
@@ -49,68 +29,91 @@ const getComponent = e => {
  		: React.createElement(DragSource("draggable", genericDragSource, collect)(genericDraggable), e.props, [])
 }
 
-const removeElement = id => {
-	const removeElement_ = (root, id) => {
-		if (UITREE[root].children[id]) delete UITREE[root].children[id];
-		else UITREE[root].children.forEach(e => removeElement_(e.id, id));
-	};
-	removeElement_(1, id);
-};
+// const squareTarget = {
+//   drop(props) {
+//     lastId+=1
+//     addChild(props.id, {
+//         type: "div",
+//         props: {style:{backgroundColor:'blue'}},
+//         children: { lastId: { type: "text", value: "it works" } }
+//       })
+//   },
+// }
 
-export const addChild = (id, child) => {
-	const addChild_ = (root, id, child) => {
-		if (root === id) {
-			lastId += 1;
-			UITREE[id].children[lastId] = child;
-		} else {
-			Object.keys(UITREE[root].children).forEach(k =>
-				addChild(k, id, child)
-			);
-		}
-	};
-	addChild_(1, id, child);
-};
+// function collect(connect, monitor) {
+//   return {
+//     connectDropTarget: connect.dropTarget(),
+//     isOver: monitor.isOver(),
+//   }
+// }
 
-setInterval(()=>addChild(1, {
-	type: "div",
-	props: null,
-	children: { 10: { type: "text", value: "addition works" } }
-}),2000)
-
-
-const squareTarget = {
-  drop(props) {
-    lastId+=1
-    addChild(props.id, {
-        type: "div",
-        props: {style:{backgroundColor:'blue'}},
-        children: { lastId: { type: "text", value: "it works" } }
-      })
-  },
-}
-
-function collect(connect, monitor) {
-  return {
-    connectDropTarget: connect.dropTarget(),
-    isOver: monitor.isOver(),
-  }
-}
+const deepCloneUITree = o=> JSON.parse(JSON.stringify(o))
 
 export class Slate extends React.Component {
-	state = {UITREE:UITREE}
+	state = {
+		UITREE:{
+			1: {
+				type: "div",
+				props: {style:{backgroundColor:'red', padding:"1em"}},
+				children: {
+					2: {
+						type: "div",
+						props: {style:{backgroundColor:'blue'}},
+						children: { 3: { type: "text", value: "it works" } }
+					},
+					4: {
+						type: "div",
+						props: null,
+						children: { 5: { type: "text", value: "deletion works" } }
+					}
+				}
+			}
+		}
+	}
 
-	componentDidMount() {
-		setInterval(()=>{addChild(1, {
-			type: "div",
-			props: null,
-			children: { 10: { type: "text", value: "addition works" } }
-		});this.setState({UITREE:UITREE})}
-		,2000)
+	// removeElement = id => {
+	// 	const removeElement_ = (root, id) => {
+	// 		if (UITREE[root].children[id]) delete UITREE[root].children[id];
+	// 		else UITREE[root].children.forEach(e => removeElement_(e.id, id));
+	// 	};
+	// 	removeElement_(1, id);
+	// };
+
+	addChild = (id, child) => {
+		console.log(' hit')
+		let clone = deepCloneUITree(this.state.UITREE)		
+		const addChild_ = (obj, id, child) => {			
+			if (obj[id]) {
+				lastId += 1;
+				obj[id].children[lastId] = child;				
+			} else {
+				console.log(obj)				
+				Object.keys(obj).forEach(k =>
+					{
+						console.log(obj)
+						console.log(k)
+						addChild_(obj[k], id, child)
+					}
+				);
+			}
+		}
+		addChild_(clone, id, child);
+		console.log(clone)
+		this.setState({UITREE:clone})
+	};
+
+
+
+	componentDidMount() {		
+		this.addChild(1, {
+						type: "div",
+						props: null,
+						children: { lastId: { type: "text", value: "dynamic addition works" } }
+					})
+		lastId+=1
 	}
 
 	render() {
-		return <div>{getUITree()}</div>
+		return <div>{getComponent(this.state.UITREE[1],null,[])}</div>
 	}
 }
-
-export const getUITree = () => getComponent(UITREE[1],null,[])
